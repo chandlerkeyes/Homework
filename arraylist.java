5865e88ac24ca0b9c5ab4fa1a06b9c8aa7bd3228
cpalme15 <cpalme15@emich.edu>
2017-04-12 17:29:23 -0400

    Update arraylist.java

--- arraylist.java
+++ arraylist.java
@@ -1,35 +1,40 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+package chandelre;
 
-public class arraylist
-{
-    public static void main(String args[])
-    {
-    Scanner keyboard = new Scanner(System.in);
-    int[] array1 = new int[4000];
-    int i;
-    int j;
-    System.out.println("Enter anywhere from 1 to 4000 integers termminate by 0");
-    for(i=0; i <3999; i++) {
-        
-        if(keyboard.nextInt()==0)
-        {
-        break;
-        }
-        else
-        {
-            array1[i]=keyboard.nextInt();
-        }
-}
-for(j=0; j<=i; j++)
-{
-System.out.println("This is forward" +array1[j]);
-for(j=i; j>=0; j--)
-{
- System.out.println("This is backward" +array1[j]);
-}       
-}       
-}  
-}    
-         
-    
+	import java.util.ArrayList;
+	import java.util.Scanner;
+
+	public class arraylist
+	{
+	
+	    public static void main(String args[])
+	    {
+	    	int i;
+	    	int j;
+	    Scanner keyboard = new Scanner(System.in);
+	    int[] array1 = new int[4000];
+	    
+	    System.out.println("Enter anywhere from 1 to 4000 integers termminate by 0");
+	  for ( i=0;i<=3999;i++)
+	  { int x=keyboard.nextInt();
+	  		if(x==0)
+	  		{
+	  			break;
+	  		}
+	  		else
+	  		{
+	  			array1[i]=x;
+	  		}
+		  
+	  }
+	for(j=0; j<=i-1; j++)
+	{
+	System.out.println("This is forward" +array1[j]);
+	}
+	for(j=i-1; j>-1; j--)
+	{
+	 System.out.println("This is backward" +array1[j]);
+	}       
+	     
+	}  
+	}    
+	    
